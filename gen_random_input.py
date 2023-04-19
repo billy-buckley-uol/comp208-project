87293da4877ea6c650f06dfe785b4e604cf6161f
billy-buckley-uol <131291475+billy-buckley-uol@users.noreply.github.com>
2023-04-19 18:39:17 +0100

    some early setup, uploading files

---
+++ gen_random_input.py
@@ -0,0 +1,4 @@
+# Generates an image with random text on it to be used as testing input
+
+def gen_random_input():
+    return
---
+++ main.py
@@ -0,0 +1,9 @@
+# Main file
+
+import cv2
+import pytesseract
+
+if __name__ == "__main__":
+    print("Starting...")
+
+    print("Execution completed with no errors.")
