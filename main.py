b50c04d82ee363c50546b097616cb8bce788db23
billy-buckley-uol <131291475+billy-buckley-uol@users.noreply.github.com>
2023-04-19 19:32:10 +0100

    first basic prototype with image_to_string

--- main.py
+++ main.py
@@ -2,8 +2,14 @@
 
 import cv2
 import pytesseract
+import numpy as np
+import PIL
+
 
 if __name__ == "__main__":
-    print("Starting...")
+    # specify which command can be used for tesseract
+    pytesseract.pytesseract.tesseract_cmd = "tesseract"
 
-    print("Execution completed with no errors.")
+    # use pytesseract to get text from image
+    text = pytesseract.image_to_string(np.array(PIL.Image.open("testimage1.png")))
+    print("Text: \n" + text)
